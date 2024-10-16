bde95a4069ee230685da8cc0aa61658d3b7f37fb
gtsorho <gerogesorho@gmail.com>
2024-10-16 17:20:35 +0000

    signin ui added

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { ClassificationComponent } from './main/classification/classification.component';
 import { HomeComponent } from './main/home/home.component';
 import { MfsInfoComponent } from './main/mfs-info/mfs-info.component';
+import { SigninComponent }   from './main/signin/signin.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path:'classify', component:ClassificationComponent},
     {path:'info', component:MfsInfoComponent},
+    {path:'signin', component:SigninComponent},
+
 
     // {
     //     path: 'main',
